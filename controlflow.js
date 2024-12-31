39c9e6ba819d655fec20d23f085d4ca8e8a24898
aaremyashwanth <111639396+Aaremyashwanth@users.noreply.github.com>
2024-12-31 09:24:17 +0000

    controlfolw

---
+++ controlflow.js
@@ -0,0 +1,22 @@
+//==,!=, ===,!== 
+// const balance=800
+// if(balance==900){
+//     console.log("balance is less thn 900");
+    
+// }else if (balance==800) {
+//     console.log("balance is less thn 1000");
+    
+// } else {
+//     console.log("balance is 800");
+// }
+
+//And operator& OR | operator
+const aadhaar=true
+const pan =false
+if(aadhaar==true&pan==true){
+console.log("one details are messing");
+
+}else{
+    console.log("check details again");
+    
+}
